fdd8ef54566b817c10c25800f9bb6d24020c0b16
John Bowdre <john@bowdre.net>
2023-12-28 13:25:49 -0600

    shorten env var names

--- images/tailscale/start.sh
+++ images/tailscale/start.sh
@@ -2,10 +2,10 @@
 trap 'kill -TERM $PID' TERM INT
 echo "Starting Tailscale daemon"
 # -state=mem: will logout and remove ephemeral node from network immediately after ending.
-tailscaled --tun=userspace-networking --state=${TAILSCALE_STATE_ARG} ${TAILSCALE_OPT} &
+tailscaled --tun=userspace-networking --state=${TS_STATE_ARG} ${TS_OPT} &
 PID=$!
-until tailscale up --authkey="${TAILSCALE_AUTH_KEY}" --hostname="${TAILSCALE_HOSTNAME}"; do
-    sleep 0.1
+until tailscale up --authkey="${TS_AUTH_KEY}" --hostname="${TS_HOSTNAME}"; do
+  sleep 0.1
 done
 tailscale status
 wait ${PID}

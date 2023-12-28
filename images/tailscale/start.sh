968d1b6ac055871d4cc3ef099e0d3d34de8b84fb
John Bowdre <john@bowdre.net>
2023-12-28 13:26:24 -0600

    configure tailscale serve is $TS_SERVE_PORT set

--- images/tailscale/start.sh
+++ images/tailscale/start.sh
@@ -8,5 +8,10 @@ until tailscale up --authkey="${TS_AUTH_KEY}" --hostname="${TS_HOSTNAME}"; do
   sleep 0.1
 done
 tailscale status
+if [ -n "${TS_SERVE_PORT}" ]; then
+  if ! tailscale serve status | grep -q "${TS_SERVE_PORT}"; then
+    tailscale serve --bg "${TS_SERVE_PORT}"
+  fi
+fi
 wait ${PID}
 wait ${PID}

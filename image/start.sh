abe2074cbc2901be24cf9f5e6692da3a61c96d28
John Bowdre <john@bowdre.net>
2023-12-29 09:42:55 -0600

    cleanup comments

--- image/start.sh
+++ image/start.sh
@@ -1,7 +1,6 @@
 #!/bin/ash
 trap 'kill -TERM $PID' TERM INT
 echo "Starting Tailscale daemon"
-# -state=mem: will logout and remove ephemeral node from network immediately after ending.
 tailscaled --tun=userspace-networking --state=${TS_STATE} ${TS_OPT} &
 PID=$!
 until tailscale up --authkey="${TS_AUTHKEY}" --hostname="${TS_HOSTNAME}"; do
